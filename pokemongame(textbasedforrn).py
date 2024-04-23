f3e5b81e0ff2f52e9082106422a4d5dd1822a58c
Crystalinxx <167729384+Crystalinxx@users.noreply.github.com>
2024-04-23 19:18:22 -0400

    Add files via upload

---
+++ pokemongame(textbasedforrn).py
@@ -0,0 +1,21 @@
+#rudimentary pokemon game.
+from random import randint
+#dialouge options
+print("You are a pokemon trainer 'Red'")
+print("Greninja is your Pokemon")
+print("Your opponent is Incineroar")
+print("1 for move")
+print("2 for move")
+print("3 for move")
+print("4 for move")
+print("items not implemented yet")
+#move selection
+move = input(":")
+if move == "1":
+    print("move1")
+elif move == "2":
+    print("move2")
+elif move == "3":
+    print("move3")
+elif move == "4":
+    print("move4")
