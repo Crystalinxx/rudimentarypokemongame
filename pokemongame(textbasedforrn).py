6d3b7e754f6fcc78f99a1510747379481b6d9f94
Crystalinxx <167729384+Crystalinxx@users.noreply.github.com>
2024-04-23 19:50:48 -0400

    Add files via upload

--- pokemongame(textbasedforrn).py
+++ pokemongame(textbasedforrn).py
@@ -1,5 +1,10 @@
 #rudimentary pokemon game.
 from random import randint
+move1 = randint(1,25)
+move2 = randint(26,50)
+move3 = randint(51,75)
+move4 = randint(1,89)
+health = 100
 #dialouge options
 print("You are a pokemon trainer 'Red'")
 print("Greninja is your Pokemon")
@@ -13,9 +18,17 @@ print("items not implemented yet")
 move = input(":")
 if move == "1":
     print("move1")
+    print("Incineroar took "+str(move1),"dmg")
+    print("Opponent health = "+str(health-move1))
 elif move == "2":
     print("move2")
+    print("Incineroar took "+str(move2),"dmg")
+    print("Opponent health = "+str(health-move2))
 elif move == "3":
     print("move3")
+    print("Incineroar took "+str(move3),"dmg")
+    print("Opponent health = "+str(health-move3))
 elif move == "4":
     print("move4")
+    print("Incineroar took "+str(move4),"dmg")
+    print("Opponent health = "+str(health-move4))
