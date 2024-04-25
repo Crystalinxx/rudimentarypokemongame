2e13d1cdac8b196a6266f5ff50e32d825278b155
Crystalinxx <167729384+Crystalinxx@users.noreply.github.com>
2024-04-24 23:07:35 -0400

    Add files via upload

--- pokemongame(textbasedforrn).py
+++ pokemongame(textbasedforrn).py
@@ -1,9 +1,16 @@
 #rudimentary pokemon game.
 from random import randint
-move1 = randint(1,25)
-move2 = randint(26,50)
-move3 = randint(51,75)
-move4 = randint(1,89)
+Crithitchance = randint(1,16)
+if Crithitchance == 16:
+    move1 = randint(1,25)*1.5
+    move2 = randint(26,50)*1.5
+    move3 = randint(51,75)*1.5
+    move4 = randint(1,89)*1.5
+else:
+    move1 = randint(1,25)
+    move2 = randint(26,50)
+    move3 = randint(51,75)
+    move4 = randint(1,89)
 health = 100
 #dialouge options
 print("You are a pokemon trainer 'Red'")
@@ -15,20 +22,24 @@ print("3 for move")
 print("4 for move")
 print("items not implemented yet")
 #move selection
-move = input(":")
+move = input("")
 if move == "1":
     print("move1")
     print("Incineroar took "+str(move1),"dmg")
     print("Opponent health = "+str(health-move1))
+    print("number was "+str(Crithitchance))
 elif move == "2":
     print("move2")
     print("Incineroar took "+str(move2),"dmg")
     print("Opponent health = "+str(health-move2))
+    print(Crithitchance)
 elif move == "3":
     print("move3")
     print("Incineroar took "+str(move3),"dmg")
     print("Opponent health = "+str(health-move3))
+    print(Crithitchance)
 elif move == "4":
     print("move4")
     print("Incineroar took "+str(move4),"dmg")
     print("Opponent health = "+str(health-move4))
+    print(Crithitchance)
